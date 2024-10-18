083ad17673fabd78318ff7e21814efe9715d397c
Daniel Zheng <daniel.zheng923125@gmail.com>
2024-10-18 15:14:16 -0400

    made slider animation and wraparound

--- script.js
+++ script.js
@@ -4,8 +4,26 @@ const arrow = document.querySelectorAll(".arrow");
 const movieList = document.querySelectorAll(".movie-list");
 
 arrow.forEach((arrow, i) =>{
+    const itemNum = movieList[i].querySelectorAll(".movie-list-item-image").length;
+    let clickCount = 0;
+
+    const barLength = 440 * itemNum;
+
+    // make it so that the slider ends perfectly
+
     arrow.addEventListener("click", () => {
-        movieList[i].style.transform = `translateX(${
-            movieList[i].computedStyleMap().get("transform")[0].x.value-270}px)`;
-    });
+        clickCount++;
+
+
+
+        if (itemNum - (4 + clickCount) >= 0){
+            movieList[i].style.transform = `translateX(${
+                movieList[i].computedStyleMap().get("transform")[0].x.value-440}px)`;
+            
+            } else{
+                movieList[i].style.transform = "translateX(0)"
+                clickCount=0;
+            }
+        });
+    
 });
