57c9233d6d3b4e79d1df9d8fb67c4ad077ccf099
Daniel Zheng <daniel.zheng923125@gmail.com>
2024-10-19 00:25:52 -0400

    added more movies/shows, footer, bar wrapper

--- script.js
+++ script.js
@@ -9,14 +9,14 @@ arrow.forEach((arrow, i) =>{
 
     const barLength = 440 * itemNum;
 
-    // make it so that the slider ends perfectly
+    // need to fix the bar resizing
 
     arrow.addEventListener("click", () => {
         clickCount++;
 
 
 
-        if (itemNum - (4 + clickCount) >= 0){
+        if (itemNum - (3 + clickCount) >= 0){
             movieList[i].style.transform = `translateX(${
                 movieList[i].computedStyleMap().get("transform")[0].x.value-440}px)`;
             
